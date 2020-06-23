1bb088d8ffa8bed2d1ebcf54e4d79707eba54976
dhruvie <dj55427@gmail.com>
2020-06-23 16:03:15 +0530

    Add .gitignore and Procfile and Ports

--- app.js
+++ app.js
@@ -161,7 +161,10 @@ app.post("/admin/editnotice", Notice.editnotice);
 
 app.get("/notices", Notice.notice);
 
-
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
